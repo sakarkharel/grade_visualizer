5fe21641af4416dc0c3e168230c25abf2d890d3a
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2025-05-17 16:35:35 +0545

    initial commit

---
+++ percent.py
@@ -0,0 +1,11 @@
+from selenium import webdriver 
+
+driver = webdriver.Chrome()
+driver.get("https://gap.westcliff.edu/")
+driver.implicity_wait(0.5)
+username_box = driver.find_element(by = By.ID, value = "username" )
+submit.button = driver.find_elemeny.t(by = B)
+
+
+
+
