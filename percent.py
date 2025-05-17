9dad15eb26304d8890ff55ec9a4f8a869ca5696f
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2025-05-17 18:42:02 +0545

    Scraping the five subjects done

--- percent.py
+++ percent.py
@@ -1,11 +1,41 @@
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
+import time 
 
 driver = webdriver.Chrome()
 driver.get("https://gap.westcliff.edu/")
-driver.implicity_wait(0.5)
-username_box = driver.find_element(by = By.ID, value = "username" )
-submit.button = driver.find_elemeny.t(by = B)
-
+driver.implicitly_wait(0.5)
+username_box = driver.find_element(by = By.ID, value = "inputName" )
+password_box = driver.find_element(by = By.ID, value = "inputPassword")
+submit_button = driver.find_element(by = By.CSS_SELECTOR, value  = "button")
+username_box.send_keys("BN210558")
+password_box.send_keys("Kharel1234##")
+submit_button.click()
+user_dropdown = driver.find_element(By.CLASS_NAME, "userbutton")
+user_dropdown.click()
+grades_link = driver.find_element(By.LINK_TEXT, "Grades")
+grades_link.click()
+time.sleep(5)  
+course1_text = driver.find_element(By.ID, value = "grade-report-overview-10498_r0_c0")
+course1_grades = driver.find_element(By.ID, value ="grade-report-overview-10498_r0_c1" )
+course2_text = driver.find_element(By.ID, value = "grade-report-overview-10498_r1_c0")
+course2_grades = driver.find_element(By.ID, value = "grade-report-overview-10498_r1_c1")
+course3_text = driver.find_element(By.ID, value = "grade-report-overview-10498_r2_c0")
+course3_grades = driver.find_element(By.ID, value = "grade-report-overview-10498_r2_c1")
+course4_text = driver.find_element(By.ID, value = "grade-report-overview-10498_r3_c0")
+course4_grades = driver.find_element(By.ID, value = "grade-report-overview-10498_r3_c1")
+course5_text = driver.find_element(By.ID, value = "grade-report-overview-10498_r4_c0")
+course5_grades = driver.find_element(By.ID, value = "grade-report-overview-10498_r4_c1")
+print(course1_text.text)
+print(course1_grades.text)
+print(course2_text.text)
+print(course2_grades.text)
+print(course3_text.text)
+print(course3_grades.text)
+print(course4_text.text)
+print(course4_grades.text)
+print(course5_text.text)
+print(course5_grades.text)
 
 
 
